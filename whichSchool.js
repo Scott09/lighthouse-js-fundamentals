5043dff57b9ab174b438b9a89301e217703c6902
Scott Appleton <Scottappleton09@gmail.com>
2019-05-25 08:44:36 +0000

    commiting looping exercises and a few calculation exercises

---
+++ whichSchool.js
@@ -0,0 +1,10 @@
+function whichSchool(age) {
+  // Your code in here ...
+  if (age < 13){
+    return "Elementary School";
+  } else if (age < 19){
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+}
